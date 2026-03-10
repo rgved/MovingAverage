843e0120e299c4ac6beaddae610a21e8baf14d69
RgvedK <155557867+rgved@users.noreply.github.com>
2026-03-10 17:30:56 +0530

    Fix indentation in return statement

--- dashboard/app.py
+++ dashboard/app.py
@@ -268,7 +268,7 @@ def build_download_csv(stock_list, tf):
 
 
 
-        return pd.DataFrame(columns=["Date", *ma_pairs.keys(), "Closing price"])
+    return pd.DataFrame(columns=["Date", *ma_pairs.keys(), "Closing price"])
 
     download_df = download_df.sort_values("Date").reset_index(drop=True)
 
